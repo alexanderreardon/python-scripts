49101d2ee4586bd46e165d450c3946d9c463f220
Alex R <alex.reardon14@gmail.com>
2018-03-25 16:10:20 -0400

    Add command line argument functionality to getLinks script. Adds support for non-specified http in URL.

--- get-links.py
+++
@@ -1,9 +0,0 @@
-import bs4 as bs
-import urllib.request
-import sys
-
-url = sys.argv[1]
-content = urllib.request.urlopen(url).read()
-soup = bs.BeautifulSoup(content)
-for link in soup.find_all('a'):
-    print(link.get('href'))
---
+++ getLinks.py
@@ -0,0 +1,27 @@
+# Originally developed for Bryant REDDay 2018 Presentation
+
+import bs4
+from urllib.request import urlopen
+import sys
+
+def getLinks(url):
+	# Adds HTTP protocol if not specified in user input:
+	if (url.startswith('http://') or url.startswith('https://')):
+		url = url
+	else:
+		url = 'http://' + url
+
+	source = urlopen(url).read()
+	soup = bs4.BeautifulSoup(source, 'lxml')
+	links = soup.find_all('a')
+	for a in links:
+		print(a.get('href'))
+
+# If URL is specified from command line, use that. Otherwise, ask the user:
+if len(sys.argv) == 1:
+	getLinks(input("Enter the web address: "))
+elif len(sys.argv) == 2:
+	getLinks(sys.argv[1])
+
+# Prevents window from closing automatically if not run from Command Line:
+input("[ Press any key to close. ]")
